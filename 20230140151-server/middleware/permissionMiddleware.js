a562ff986e4e5b6239026832ff2ca25630a7082b
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-10-19 15:37:37 +0700

    membuat Tugas 3

---
+++ 20230140151-server/middleware/permissionMiddleware.js
@@ -0,0 +1,21 @@
+// Middleware untuk menambahkan data user dummy
+exports.addUserData = (req, res, next) => {
+  console.log('Middleware: Menambahkan data user dummy...');
+  req.user = {
+    id: 123,
+    nama: 'User Karyawan', // ✅ perbaikan tanda kutip
+    role: 'admin'       // ubah ke 'admin' kalau mau tes route report
+  };
+  next();
+};
+
+// Middleware untuk cek apakah user adalah admin
+exports.isAdmin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    console.log('Middleware: Izin admin diberikan.');
+    next();
+  } else {
+    console.log('Middleware: Gagal! Pengguna bukan admin.');
+    return res.status(403).json({ message: 'Akses ditolak: Hanya untuk admin' });
+  }
+};
---
+++ TUGAS3/Tugas3.md
@@ -0,0 +1,23 @@
+# Tugas 3
+
+## 1️⃣ POST /api/presensi/check-in
+- Berhasil check-in
+![Tampilan check-in](ss/checkin.png)
+
+- check-in lebih dari 1 kali
+![Tampilan check-in lagi](ss/checkinlagi.png)
+
+
+## 2️⃣ POST /api/presensi/check-out
+- Berhasil check-out
+![Tampilan check-out](ss/checkout.png)
+
+- check-out lebih dari 1 kali
+![Tampilan check-out lagi](ss/checkoutlagi.png)
+
+## 3️⃣ GET /api/reports/daily
+- Admin
+![Tampilan Admin](ss/admin.png)
+
+- Bukan admin
+![Tampilan bukan Admin](ss/bukanadmin.png)

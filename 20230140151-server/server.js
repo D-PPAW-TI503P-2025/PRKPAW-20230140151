0946f9234f49845f72273ddedde95b6c96947528
“Kuroyukimaru” <oranggabut712@gmail.com>
2025-11-06 15:51:02 +0700

    Tugas6

--- 20230140151-server/server.js
+++ 20230140151-server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,6 +9,7 @@ const morgan = require("morgan");
 // Impor router
 const presensiRoutes = require("./routes/presensi");
 const reportRoutes = require("./routes/reports");
+const authRoutes = require('./routes/auth');
 
 // Middleware
 app.use(cors());
@@ -23,6 +26,7 @@ const ruteBuku = require("./routes/books");
 app.use("/api/books", ruteBuku);
 app.use("/api/presensi", presensiRoutes);
 app.use("/api/reports", reportRoutes);
+app.use('/api/auth', authRoutes);
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
 });
---
+++ TUGAS6/Tugas6.md
@@ -0,0 +1,8 @@
+## 1. mahasiswa
+![Mahasiswa](ss/POSTmahasiswa.png)
+
+## 2. Admin
+![Admin](ss/POSTmahasiswa.png)
+
+## 3. Login admin
+![Login](ss/loginadatoken.png)
